1a7a1b83a16d80a31f764937e3d522c819f9ccfc
matiasfair14 <matiastorrejon14@gmail.com>
2021-12-01 01:55:31 -0300

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+namespace testCountriesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+     
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            string url = "https://restcountries.com/v3.1/all";
+            HttpClient client = new HttpClient();
+            var httpResponse = await client.GetAsync(url);
+            if(httpResponse.IsSuccessStatusCode)
+            {
+                var content = await httpResponse.Content.ReadAsStringAsync();
+            return content;
+
+            }
+            return "error";
+           
+        }
+    }
+}
